7d88ded9dca2bce01be3bec39f52232c6b2f27fa
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-07-07 11:05:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/2acf37dd041dc8ca
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2acf37dd041dc8cae6fc2.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5651594253').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5651594253, 866072531').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
